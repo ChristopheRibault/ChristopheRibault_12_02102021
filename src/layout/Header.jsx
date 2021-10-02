f79df51a9f15d2ffc9f9e3bf6bf04d67448fe5cf
ChristopheRibault <c.ribault.dev@gmail.com>
2021-10-02 17:19:29 +0200

    style: formatting header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,29 @@
 import { Component } from 'react';
 import { Navbar } from '.';
 import { Logo } from '../components';
+import style from 'styled-components';
+
+const StyledHeader = style.header`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+  gap: 3rem;
+  width: 100%;
+  background-color: #020203;
+  color: #fff;
+  padding: 1em;
+  margin: 0;
+`;
 
 class Header extends Component {
+
   render() {
     return (
-      <header>
+      <StyledHeader>
         <Logo />
         <Navbar />
-      </header>
+      </StyledHeader>
     );
   }
 }
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,15 +1,30 @@
 import { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import style from 'styled-components';
+
+const Nav = style.nav`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-contents: space-evenly;
+  gap: 3rem;
+`;
+
+const StyledNavLink = style(NavLink)`
+  text-decoration: none;
+  color: #fff;
+  font-weight: 500;
+`;
 
 class Navbar extends Component {
   render() {
     return (
-      <nav>
-        <NavLink exact to='/'>Accueil</NavLink>
-        <NavLink exact to='/profil'>Profil</NavLink>
-        <NavLink exact to='/settings'>Réglage</NavLink>
-        <NavLink exact to='/community'>Communauté</NavLink>
-      </nav>
+      <Nav>
+        <StyledNavLink exact to='/'>Accueil</StyledNavLink>
+        <StyledNavLink exact to='/profil'>Profil</StyledNavLink>
+        <StyledNavLink exact to='/settings'>Réglage</StyledNavLink>
+        <StyledNavLink exact to='/community'>Communauté</StyledNavLink>
+      </Nav>
     );
   }
 }
